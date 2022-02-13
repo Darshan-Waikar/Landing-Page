d9059cd09aac40a57847195205a4616667c9b089
Darshan <waikardarshan@gmail.com>
2022-02-13 19:48:02 +0530

    Initial

---
+++ src/components/YellowSub.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./YellowSub.css";
+
+function YellowSub() {
+    return ( <
+        div className = "yellow" >
+        <
+        div className = "yellow-container" > < /div> <
+        /div>
+    );
+}
+
+export default YellowSub;
